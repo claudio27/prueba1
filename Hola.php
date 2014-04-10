7cdaf219d0ec8fe4edf5963a110bbfc705435f71
Claudio <claudioserrano2@gmail.com>
2014-04-10 00:51:55 -0400

    Un nuevo llamado de objeto

--- Hola.php
+++ Hola.php
@@ -14,8 +14,13 @@ print "Hola mundo \n";
 
 $claudio = new Persona();
 $otro = new Persona();
+$any = new Persona();
+
 
 $claudio->Saludo();
 $otro->Saludo();
+$any->Saludo();
+// otro objeto mas
+
 
 ?>
